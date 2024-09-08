ae500b634faae6c0ea2c100bb2dcdb101327bb4c
Júnior Nonato <126834509+junior-nonato@users.noreply.github.com>
2024-09-07 21:20:40 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que marcou história ao se tornar a maior medalhista olímpica do país. Com uma carreira brilhante, marcada por dedicação e talento, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e determinação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carismático, ela conquistou o mundo do skate e inspirou uma nova geração de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "judô judo judoka"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua determinação e técnica impecável a consagraram como uma das maiores judocas da atualidade.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ginástica ginastica ouro"
+    }
+];
